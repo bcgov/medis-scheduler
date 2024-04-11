5927b34c0b7f001baf0ea71edc5f704ca524963a
tpluzhni <tatiana.pluzhnikova@cgi.com>
2024-04-11 09:14:05 -0700

    rename Dag tag

--- dags/MEDIS-ETL.py
+++ dags/MEDIS-ETL.py
@@ -40,8 +40,8 @@ with DAG(
     start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
     catchup=False,
     dagrun_timeout=datetime.timedelta(minutes=60),
-    tags=["example", "example2"],
-    params={"example_key": "example_value"},
+    tags=["etl", "medis"],
+   # params={"example_key": "example_value"},
 ) as dag:
     etl_job_task = KubernetesJobOperator(
         task_id='MEDIS_file_upload',
