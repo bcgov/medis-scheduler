df0d530075e887154f25523a6dd73ed270019160
tpluzhni <tatiana.pluzhnikova@cgi.com>
2024-04-26 15:19:30 -0700

    test email

--- dags/email_test.py
+++ dags/email_test.py
@@ -44,8 +44,8 @@ with DAG(
 ) as dag:
     send_email = EmailOperator( 
         task_id='send_email', 
-        to='tatiana.pluzhnikova@cgi.com', 
-        subject='ingestion complete', 
+        to='tatiana.pluzhnikova@cgi.com', 'evgeny.pluzhnikov@cgi.com',
+        subject='privet ot moego DAG', 
         html_content="Date: {{ ds }}",
         trigger_rule="all_failed"
     )
