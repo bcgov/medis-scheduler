c6e3929bf69fbc4a3fd698e74e730b5f036df1b8
tpluzhni <tatiana.pluzhnikova@cgi.com>
2024-05-07 09:56:25 -0700

    added failed notifications

--- dags/MEDIS-ETL.py
+++ dags/MEDIS-ETL.py
@@ -49,6 +49,42 @@ with DAG(
         job_template_file='{{var.value.medis_job}}',
     )
 
+# Function to generate HTML content for email
+    def generate_html(failed_ids):
+        current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S UTC")
+        html_content = "<html><head></head><body><h1>Airflow run at %s failed</h1><p>Automatically generated message in case of failure.</p><h2>Failed Task IDs</h2><ul>" % current_time
+        for failed_id in failed_ids:
+            html_content += f"<li>{failed_id}</li>"
+        html_content += "</ul><h4>Please access Airflow and review tasks run.</h4></body></html>"
+        print(html_content)
+        return html_content
+
+    def get_failed_ids_send_email(ds=None, **kwargs):
+        ti = kwargs['ti']
+        dag_run = kwargs['dag_run']
+        # Get all tasks that are upstream of this task
+        upstream_task_ids = ti.task.get_flat_relative_ids(upstream=True)
+        # Get list of all tasks that have failed for this DagRun
+        failed_task_instances = dag_run.get_task_instances(state='failed')
+        # Get intersection of the sets to get upstream tasks that failed
+        failed_upstream_task_ids = upstream_task_ids.intersection(
+            [task.task_id for task in failed_task_instances])
+        print(f"Upstream tasks that failed: {failed_upstream_task_ids}")
+        # If no upstream tasks have failed, skip this task
+        if len(failed_upstream_task_ids) == 0:
+            raise AirflowSkipException("No upstream tasks have failed")
+        # If there are failed upstream tasks, send an email with the failed task IDs
+        elif len(failed_upstream_task_ids) > 0:
+            send_email(
+                to=Variable.get("ETL_email_list_alerts"),
+                subject='subject',
+                html_content=generate_html(failed_upstream_task_ids),
+            )
+        return failed_upstream_task_ids
+
+    failed_tasks_notification = PythonOperator(
+        task_id="Failed_Tasks_Notification", python_callable=get_failed_ids_send_email, trigger_rule="all_done")
+
     start_ytd_extract = EmptyOperator(
         task_id="Start_LTC_YTD_Extract",
     )
@@ -96,13 +132,6 @@ with DAG(
         headers={"Content-Type": "application/json"},
     )
 
-    start_ytd_extract >> ytd_fha_task >> etl_job_task
-    start_ytd_extract >> ytd_iha_task >> etl_job_task
-    start_ytd_extract >> ytd_viha_task >> etl_job_task
-    start_ytd_extract >> ytd_nha_task >> etl_job_task
-    start_ytd_extract >> ytd_vch_task >> etl_job_task
-
-
     
     facility_fha_task = HttpOperator(
         task_id='LTC_Facility_Information_Fraser',
@@ -112,7 +141,6 @@ with DAG(
         headers={"Content-Type": "application/json"},
     )
 
-    facility_fha_task >> start_ytd_extract
 
     facility_iha_task = HttpOperator(
         task_id='LTC_Facility_Information_Interior',
@@ -122,7 +150,6 @@ with DAG(
         headers={"Content-Type": "application/json"},
     )
 
-    facility_iha_task >> start_ytd_extract    
     
     facility_viha_task = HttpOperator(
         task_id='LTC_Facility_Information_Island',
@@ -132,7 +159,6 @@ with DAG(
         headers={"Content-Type": "application/json"},
     ) 
 
-    facility_viha_task >> start_ytd_extract
 
     facility_nha_task = HttpOperator(
         task_id='LTC_Facility_Information_Northern',
@@ -142,7 +168,6 @@ with DAG(
         headers={"Content-Type": "application/json"},
     )
 
-    facility_nha_task >> start_ytd_extract
 
     facility_vch_task = HttpOperator(
         task_id='LTC_Facility_Information_Vancouver',
@@ -152,17 +177,22 @@ with DAG(
         headers={"Content-Type": "application/json"},
     )
 
-    facility_vch_task >> start_ytd_extract
 
     start_facility_extract = EmptyOperator(
         task_id="Start_LTC_Facility_Extract",
     )
 
-    start_facility_extract >> facility_fha_task
-    start_facility_extract >> facility_iha_task
-    start_facility_extract >> facility_viha_task
-    start_facility_extract >> facility_nha_task
-    start_facility_extract >> facility_vch_task
+    start_facility_extract >> facility_fha_task >> failed_tasks_notification >> start_ytd_extract
+    start_facility_extract >> facility_iha_task >> failed_tasks_notification >> start_ytd_extract
+    start_facility_extract >> facility_viha_task >> failed_tasks_notification >> start_ytd_extract
+    start_facility_extract >> facility_nha_task >> failed_tasks_notification >> start_ytd_extract
+    start_facility_extract >> facility_vch_task >> failed_tasks_notification >> start_ytd_extract
+
+    start_ytd_extract >> ytd_fha_task >> failed_tasks_notification >> etl_job_task
+    start_ytd_extract >> ytd_iha_task >> failed_tasks_notification >> etl_job_task
+    start_ytd_extract >> ytd_viha_task >> failed_tasks_notification >> etl_job_task
+    start_ytd_extract >> ytd_nha_task >> failed_tasks_notification >> etl_job_task
+    start_ytd_extract >> ytd_vch_task >> failed_tasks_notification >> etl_job_task
 
 
 
