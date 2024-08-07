d3dac9a2e30fe1a64666cae710afe7ced08e5e29
tpluzhni <tatiana.pluzhnikova@cgi.com>
2024-08-07 15:50:24 -0700

    added deferrable=True, to fix wait_until_job_complete

--- dags/MEDIS-ETL.py
+++ dags/MEDIS-ETL.py
@@ -200,6 +200,7 @@ with DAG(
         task_id='Check_LTC_Shared_Folder',
         job_template_file='{{var.value.medis_emtydir_job}}',
         wait_until_job_complete=True,
+        deferrable=True,
     )
 
     start_facility_extract = EmptyOperator(
